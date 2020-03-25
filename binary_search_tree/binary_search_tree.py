466eb2849f5929da8d842fd430dda1c4c54a53b6
XAmi101 <famousvacancy@gmail.com>
2020-03-25 18:51:55 -0400

    Day 3 insert and contains complete, cworking on max

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -12,12 +12,49 @@ class BinarySearchTree:
 
     # Insert the given value into the tree
     def insert(self, value):
-        pass
+        # PLAN
+        #looks to see if node value is greater than self.value, is so go left
+        if value < self.value:
+            #if so go left side and check if the left is empty
+            if self.left is None:
+                #if so the node value then becomes the new value and return it 
+                self.left = BinarySearchTree(value)
+                return
+            else:
+                #but if there is a value then inserts the node in the left 
+                self.left.insert(value)
+
+        #But if node value is less than or equal to self.value
+        if value >= self.value:
+            #go right and check if the right side is empty
+            if self.right is None:
+                #if so node value becomes the new value for the right
+                self.right = BinarySearchTree(value)
+                return
+            else:
+                #but if there is a value then it inserts it in the right side
+                self.right.insert(value)
 
     # Return True if the tree contains the value
     # False if it does not
     def contains(self, target):
-        pass
+        # PLAN
+        # check if the self value is the same as target
+            # return true
+        # else if the tree value is the greater than the value in the left and target
+            # return that the left side contains the target
+        # else if the tree value is the less than the value in the right and target
+            # return that the right side contains the target
+        # else return false
+
+        if self.value == target:
+            return True
+        elif self.left and target < self.value:
+            return self.left.contains(target)
+        elif self.right and target > self.value:
+            return self.right.contains(target)
+        else:
+            return False
 
     # Return the maximum value found in the tree
     def get_max(self):
