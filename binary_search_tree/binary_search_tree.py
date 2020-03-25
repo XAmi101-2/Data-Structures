54a0bea7cb18049cfc9e0f1f467ee48cdf77265c
XAmi101 <famousvacancy@gmail.com>
2020-03-25 19:42:29 -0400

    Day 3 get max complete, working on for each

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -13,7 +13,7 @@ class BinarySearchTree:
     # Insert the given value into the tree
     def insert(self, value):
         # PLAN
-        #looks to see if node value is greater than self.value, is so go left
+        #looks to see if node value is greater than self.value, is so recurse left
         if value < self.value:
             #if so go left side and check if the left is empty
             if self.left is None:
@@ -21,7 +21,7 @@ class BinarySearchTree:
                 self.left = BinarySearchTree(value)
                 return
             else:
-                #but if there is a value then inserts the node in the left 
+                #but if there is a value then inserts the value in the left node
                 self.left.insert(value)
 
         #But if node value is less than or equal to self.value
@@ -39,26 +39,44 @@ class BinarySearchTree:
     # False if it does not
     def contains(self, target):
         # PLAN
-        # check if the self value is the same as target
+        # check/compare if the node is empty
+        # check/compare if the node value is the same as target search value
             # return true
-        # else if the tree value is the greater than the value in the left and target
-            # return that the left side contains the target
-        # else if the tree value is the less than the value in the right and target
-            # return that the right side contains the target
-        # else return false
-
+        # else if target search value is less than the node value (go left ) and if there is node.left  
+            # return  the left side contains the target
+        # else if target is  greater than the tree value recurse right and if there is node.right 
+            # return  the right side contains the target
+        ## else return false
+        
+        if self.value == None:
+            return False
         if self.value == target:
             return True
-        elif self.left and target < self.value:
-            return self.left.contains(target)
-        elif self.right and target > self.value:
-            return self.right.contains(target)
-        else:
-            return False
+        # elif target < self.value and self.left :
+            #  return self.left.contains(target)
+        # elif self.right and target > self.value:
+        #     return self.right.contains(target)
+        elif target < self.value:
+            if self.left:
+                return self.left.contains(target)   
+        elif target > self.value:
+            if self.right:
+                return self.right.contains(target)   
+        # else:
+        #     return False
 
     # Return the maximum value found in the tree
     def get_max(self):
-        pass
+        # PLAN
+        # check if there is a value on the right 
+            # if so return get max value on right
+        # else return node.value
+
+        if self.right:
+            return self.right.get_max()
+        else:
+            return self.value
+        
 
     # Call the function `cb` on the value of each node
     # You may use a recursive or iterative approach
